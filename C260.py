55a399fde8b589b85f7b60f9e4d954ae0dc24d63
Shaurya Jain <jainshaurya774@gmail.com>
2022-10-04 10:43:15 +0530

    first commit

---
+++ C260.py
@@ -0,0 +1,23 @@
+from etherpad_lite import EtherpadLiteClient
+from datetime import datetime
+etherclient = EtherpadLiteClient(base_params={'apikey':'5ff7ea9032814c34a8f8daf05038aa9d48d29052678ca34eb40c69a981b118f9'})
+
+new_pad = etherclient.createPad(padID = 'mynewpad', text='Hello everyone,How are you' )
+print('your new pad is :',new_pad)
+
+createAuthor = etherclient.createAuthor(name='CloudUser')
+print(createAuthor)
+
+padcount = etherclient.padUsersCount(padID ='mynewpad')
+print(padcount)
+
+timestamp = etherclient.getLastEdited(padID = 'mynewpad')
+print(timestamp)
+lastedit = timestamp['lastEdited']
+
+date_conversion = datetime.fromtimestamp(lastedit/1000.0)
+print("date_time:", date_conversion)
+
+
+delete_pad = etherclient.deletePaf(padID='mynewpad')
+print(delete_pad)
